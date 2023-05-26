e49225b353591aa21e16b08e4f77e35fef3deb2b
Nate Wong <natewong1@gmail.com>
2023-05-25 22:50:20 -0400

    Change email icon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import type { IconType } from "react-icons"
 import { SiGithub, SiLinkedin } from "react-icons/si"
-import { IoMail } from "react-icons/io5"
+import { HiMail } from "react-icons/hi"
 
 export default function Footer() {
   return (
@@ -29,7 +29,7 @@ export default function Footer() {
           icon={SiLinkedin}
           href="https://www.linkedin.com/in/nate-wong-929b28191/"
         />
-        <FooterBtn icon={IoMail} href="mailto:natewong1@gmail.com" />
+        <FooterBtn icon={HiMail} href="mailto:natewong1@gmail.com" />
       </div>
     </div>
   )
