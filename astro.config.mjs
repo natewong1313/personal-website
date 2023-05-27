7fd79315a5a5d8f714d708c1e658ca694581e320
Nate Wong <natewong1@gmail.com>
2023-05-27 19:14:47 -0400

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: cloudflare()
-});
+})
