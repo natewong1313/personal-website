11f8a1c1956f7be7180f35d324f5e9133fdc1407
Nate Wong <natewong1@gmail.com>
2023-05-27 19:05:52 -0400

    Configure for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
-import react from "@astrojs/react"
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
-})
+  adapter: cloudflare()
+});
